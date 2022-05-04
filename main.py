424b72d61e0259bba1d8f57c640e4557e334789d
Jimmy Zhang <jiemingjimmy@gmail.com>
2022-05-04 16:08:33 -0500

    world class and camera object

--- main.py
+++ main.py
@@ -1,93 +1,57 @@
+from re import T
 from readline import redisplay
+from site import removeduppaths
 from PIL import Image
 import numpy as np
+from util import *
+from camera import *
+import math
 
-class Vec3:
-    def __init__(self, arr):
-        assert(arr.shape == (3,))
-        self.coor = arr
+class Sphere():
+    def __init__(self, center, radius):
+        self.center = center
+        self.radius = radius
 
-    @classmethod
-    def from_val(cls, x,y,z):
-        arr = np.array([x,y,z])
-        return Vec3(arr)
+    def hit(self, ray, t_min, t_max):
+        oc = ray.origin - self.center
+        a = ray.direction.len_squared()
+        half_b = Vec3.dot(oc, ray.direction)
+        c = oc.len_squared() - self.radius*self.radius
+        discriminant = half_b*half_b - a*c
+        if (discriminant < 0):
+            return False
+        sqrtd = math.sqrt(discriminant);
 
-    @property
-    def x(self):
-        return self.coor[0]
-    @property
-    def y(self):
-        return self.coor[1]
-    @property
-    def z(self):
-        return self.coor[2]
+        # Find the nearest root that lies in the acceptable range.
+        root = (-half_b - sqrtd) / a
+        if root < t_min or t_max < root:
+            root = (-half_b + sqrtd) / a
+            if root < t_min or t_max < root:
+                return False
 
-    @classmethod
-    def dot(cls, vec1, vec2):
-        return np.dot(vec1.coor, vec2.coor)
+        t = root
+        p = ray.at(t)
+        normal = (p - self.center) / self.radius
+        return [t, p, normal]
+        
 
-    def unwrap(self):
-        return self.coor
+class World():
+    hittables = []
+    def add_object(self, object):
+        self.hittables.append(object)
+    def hit(self, ray, t_min):
+        ret = False
+        closest_so_far = 99999
 
-    def __add__(self, other):
-        return Vec3(self.coor + other.coor)
+        for obj in self.hittables:
+            tryhit = obj.hit(ray, t_min, closest_so_far)
+            if tryhit:
+                [t, p, normal] = tryhit
+                ret = [t, p, normal]
+                closest_so_far = t
+        return ret
+                
 
-    def __mul__(self, other):
-        return Vec3(self.coor * other)
-    def __rmul__(self, other):
-        return self.__mul__(other)
-
-
-    def __truediv__(self, other):
-        return Vec3(self.coor / other)
-
-    def __sub__(self, other):
-        return Vec3(self.coor - other.coor)        
-
-    def __str__(self):
-        return f"({self.coor})"
-
-class Point3(Vec3):
-    def __init__(self, x, y, z):
-        Vec3.__init__(self, x, y, z)
-    def __init__(self, arr):
-        Vec3.__init__(arr)
-
-
-
-class Ray:
-    def __init__(self, origin, direction):
-        assert(isinstance(origin, Vec3))
-        assert(isinstance(direction, Vec3))
-
-        self._origin = origin
-        self._direction = direction
-
-    @property
-    def origin(self):
-        return self._origin
-
-    @property
-    def direction(self):
-        return self._direction
-
-    def unit_direction(self):
-        maxv = np.abs(self._direction.coor).max()
-        return self._direction / maxv
-
-    def shoot(self, t):
-        return Vec3(self._origin + t*self._direction)
-
-
-
-
-def hit_sphere(center, radius, ray):
-    oc = ray.origin - center
-    a = Vec3.dot(ray.direction, ray.direction)
-    b = 2.0 * Vec3.dot(oc, ray.direction)
-    c = Vec3.dot(oc, oc) - radius*radius
-    discriminant = b*b - 4*a*c
-    return (discriminant > 0)
 
 def print_image(data):
     data.astype(int)
@@ -101,31 +65,26 @@ if __name__ == "__main__":
     image_width = 400
     image_height = int((image_width / aspect_ratio))
     img = np.zeros((image_height, image_width, 3), dtype=np.uint8)
-    print(img.shape)
-    # data[0:256, 0:256] = [255, 0, 0] # red patch in upper left
 
     #camera
-    viewport_height = 2
-    viewport_width = aspect_ratio * viewport_height
-    focal_length = 1
-    origin = Vec3.from_val(0,0,0)
-    horizontal = Vec3.from_val(viewport_width,0,0)
-    vertical = Vec3.from_val(0, viewport_height,0)
-    lower_left_corner = origin - horizontal/2 - vertical/2 - Vec3.from_val(0,0,focal_length)
+    camera = Camera(aspect_ratio)
 
-    print(lower_left_corner)
+    world = World()
+    world.add_object(Sphere(Vec3.from_val(0,0,-1), 0.5))
+    world.add_object(Sphere(Vec3.from_val(0,-100.5,-1), 100))
 
+    print(f"Generating image {image_height} tall and {image_width} wide")
     for j in range(image_height-1, -1, -1):
         print(f"row {j}")
         for i in range(image_width-1, -1, -1):
             v = j / (image_height-1)
             u = i / (image_width-1)
-            direction = lower_left_corner + u*horizontal + v*vertical - origin
+            ray = camera.get_ray(u, v)
 
-            ray = Ray(origin, direction)
-            center = Vec3.from_val(0,0,-1)
-            if hit_sphere(Vec3.from_val(0,0,-1), 0.5, ray):
-                color = Vec3.from_val(0,0,0)
+            pt = world.hit(ray,0)
+            if pt:
+                [t, p, N] = pt
+                color = 0.5*(N + 1)
 
             else:
                 ray_dir = ray.unit_direction()
@@ -133,9 +92,9 @@ if __name__ == "__main__":
                 color = Vec3.from_val(1, 1, 1)*(1.0-t) + Vec3.from_val(0.5, 0.7, 1.0)*t
 
             color *= 255
-            img[j,i] = color.unwrap()
+            # print(f" u {u} v {v}, j {j} v {i}| t {t}")
+            img[-j+(image_height-1),i] = color.unwrap()
 
-            # print(f" u {u} v {v}| color {color}")
-
+            # input()
         print_image(img)
 
