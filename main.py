80016766b280976b781ecbf8388f4d559d3ba9d3
Jimmy Zhang <jiemingjimmy@gmail.com>
2022-05-04 22:52:02 -0500

    BBs done

--- main.py
+++ main.py
@@ -47,7 +47,11 @@ def ray_color(ray, world, depth):
         [t, p, N, front_face, obj] = pt
         bounced_ray, attenutation = obj.material.scatter(ray, N, p, front_face)
 
+        if any(v == 0 for v in bounced_ray.direction.unwrap()):
+            return Vec3(0,0,0)
+
         return attenutation * ray_color(bounced_ray, world, depth-1)
+        
     ray_dir = ray.unit_direction()
     t = 0.5*(ray_dir.y + 1.0)
     return Vec3(1, 1, 1)*(1.0-t) + Vec3(0.5, 0.7, 1.0)*t
@@ -69,17 +73,27 @@ def run():
         aspect_ratio = aspect_ratio)
 
     world = World()
+    world.add_object(Sphere(
+        Vec3(0,-1000,0), 1000, Lambertian(Vec3(0.5, 0.5, 0.5))
+        ))
     world.add_object(Sphere(Vec3( 0.0, -100.5, -1.0), 100.0, Lambertian(Vec3(0.8,0.8,0))))
-    world.add_object(Sphere(Vec3( 0.0,    0.0, -1.0),   0.5, Lambertian(Vec3(0.7,0.3,0.3))))
-    world.add_object(Sphere(Vec3(-1.0,    0.0, -1.0),   0.5, Dielectric(2)))
-    world.add_object(Sphere(Vec3( 1.0,    0.0, -1.0),   0.5, Metal(Vec3(0.8,0.6,0.2))))
+    # world.add_object(Sphere(Vec3( 0.0,    0.0, -1.0),   0.2, Lambertian(Vec3(0.7,0.3,0.3))))
+    # world.add_object(Sphere(Vec3(-1.0,    0.0, -1.0),   0.2, Dielectric(2)))
+    # world.add_object(Sphere(Vec3( 1.0,    0.0, -1.0),   0.2, Metal(Vec3(0.8,0.6,0.2))))
 
+    # for _ in range(5):
+    #     rand = random.uniform(0,1)
+    #     color = Vec3(rand,rand,rand)
+    #     world.add_object(Sphere(Vec3( 1.0,    random.uniform(0,3), random.uniform(-1,-3)),   0.2, Metal(color)))
+
+    world.create_BoundingHierarchy()
 
     print(f"Generating image {image_height} tall and {image_width} wide")
     img = np.zeros((image_height, image_width, 3), dtype=np.uint8)
 
     for j in range(image_height-1, -1, -1):
-        print(f"row {j}")
+        if j%10 == 0:
+            print(f"{100*(1-j/image_height)}% done...")
         for i in range(image_width-1, -1, -1):
 
             color = Vec3(0,0,0)
@@ -89,6 +103,7 @@ def run():
                 u = (i + random.uniform(0,1)) / (image_width-1)
 
                 ray = camera.get_ray(u, v)
+
                 color += ray_color(ray, world, ray_depth)
 
             write_color(image_height, j, i , color, img, samples_per_pixel)
